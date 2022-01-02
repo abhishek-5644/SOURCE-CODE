76f5b87c0fcd0278e6c964312ae6773aee761de5
Abhishek jha <Shreyashtivedi@gmail.com>
2022-01-02 20:24:26 +0530

    first commit

---
+++ Cpp/sizeOf.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter value of a:\t";
+    int a;
+    cin>>a;
+    cout<<"  " <<endl;
+    cout<<"Size of int is :\t"<<sizeof(int);
+
+    return 0;
+}
